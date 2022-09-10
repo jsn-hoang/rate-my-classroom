b18c1538d75815b23553dd80f7fa9178663e9599
Jason Hoang <jhoang2@ualberta.ca>
2022-09-10 13:35:54 -0600

    ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Search from './search';
+import Navbar from './navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Jashwanth!</h1>
+      <Navbar />
+      <Search />
     </div>
   );
 }
---
+++ frontend/src/navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Navbar = () => (
+  <div className="navbar">
+    <a href="#top" className="navbar-name">RateMyClassroom</a>
+  </div>
+);
+
+export default Navbar;
