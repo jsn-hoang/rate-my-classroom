fd850b38e6d4422427b230df22dac825db1335b0
Theo <eggert@ualberta.ca>
2022-09-10 14:29:26 -0600

    Connecting backend and frontend

--- frontend/src/search.jsx
+++ frontend/src/search.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import {getClassroom} from './service';
 
-const Search = () => (
-  <form action="/" method="get">
-    <label htmlFor="header-search">
-      <input
-        type="text"
-        id="header-search"
-        placeholder="Search for a classroom"
-        name="s"
-      />
-    </label>
-    <button type="submit">Search</button>
-  </form>
-);
+const Search = () => {
+  const handleSubmit = async () => {
+    const data = await getClassroom('etlc');
+    console.log(data);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="header-search">
+        <input
+          type="text"
+          id="header-search"
+          placeholder="Search for a classroom"
+          name="s"
+        />
+      </label>
+      <button type="submit">Search</button>
+    </form>
+  );
+};
 
 export default Search;
