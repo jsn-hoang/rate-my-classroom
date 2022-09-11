bade22c3b32eacb22d78c75779ed1184304c1ec8
Marc André Haley <hmarcand@ualberta.ca>
2022-09-10 23:08:05 -0600

    commit before pull

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,18 @@ import './App.css';
 import Search from './search';
 import Navbar from './navbar';
 import { Classroom } from './Classroom';
-import { Routes } from 'react-router-dom';
-import { Route } from 'react-router-dom';
+import { Link, Routes, Route } from 'react-router-dom';
 //import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<><Navbar/><Search/></>}/>
-      <Route path="/classrooms/:id" element={<><Navbar/><Classroom/></>}/>
-    </Routes>
+    <>
+      <Link to="/classrooms/2">class 2</Link>
+      <Routes>
+        <Route path="/" element={<><Navbar/><Search/></>}/>
+        <Route path="/classrooms/:id" element={<><Navbar/><Classroom/></>}/>
+      </Routes>
+    </>
   )
 }
 
