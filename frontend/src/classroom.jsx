c6905df907849ce34a75e015107cf6020921f674
xichen1 <xichen.pan@gmail.com>
2022-09-11 09:03:49 -0600

    sync

--- frontend/src/classroom.jsx
+++ frontend/src/classroom.jsx
@@ -1,36 +1,12 @@
 import React from 'react';
 import { useParams } from "react-router-dom"
-import './classroom.css'
+import { getReviewList } from "./service"
 
-export function Classroom(){
+const Classroom = async() => {
     const { id } = useParams()
-    return <h1>Classroom {id}</h1>
+    const reviewList = await getReviewList(id)
+    console.log(reviewList[0].rating)
+    return <h1>Classroom { id }</h1>
 }
 
-
-// styling for later use
-{/* <div>
-    <h1>ETLC E1-007</h1> <!--Classroom name var goes here-->
-
-    <!-- acquired from w3schools -->
-    <div class="row">
-        <div class="leftcolumn">
-            <div class="card">
-                <h1>1/5</h1> <!--rating var goes here-->
-                <h2>Dis class is dope yo</h2> <!--description var goes here-->
-            </div>
-            <div class="card">
-                <h1>1/5</h1>
-                <h2>Dis class is dope yo</h2>
-            </div>
-            <div class="card">
-                <h1>1/5</h1>
-                <h2>Dis class is dope yo</h2>
-            </div>
-            <div class="card">
-                <h1>1/5</h1>
-                <h2>Dis class is dope yo</h2>
-            </div>
-        </div>
-    </div>
-</div> */}
+export default Classroom;
