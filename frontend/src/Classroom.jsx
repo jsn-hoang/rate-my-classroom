5d68825d8ede6cbe0bc99fd0b7537cdfb1cc85c5
Marc André Haley <hmarcand@ualberta.ca>
2022-09-11 01:38:49 -0600

    gn

--- frontend/src/Classroom.jsx
+++ frontend/src/Classroom.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { useParams } from "react-router-dom"
+import { getReviewList } from "./service"
 
-export function Classroom(){
+const Classroom = async() => {
     const { id } = useParams()
-    return <h1>Classroom {id}</h1>
+    const reviewList = await getReviewList(id)
+    console.log(reviewList[0].rating)
+    return <h1>Classroom { id }</h1>
 }
+
+export default Classroom;
