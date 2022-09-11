cac2a81c8c89dc088cc894d70b6b55cadf7bfed5
xichen1 <xichen.pan@gmail.com>
2022-09-11 09:11:10 -0600

    getreviewlist

--- frontend/src/classroom.jsx
+++ frontend/src/classroom.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { useParams } from "react-router-dom"
 import { getReviewList } from "./service"
 
-const Classroom = async() => {
+const Classroom = () => {
     const { id } = useParams()
-    const reviewList = await getReviewList(id)
-    console.log(reviewList[0].rating)
-    return <h1>Classroom { id }</h1>
+    const [reviewList, setReviewList] = useState([])
+    useEffect(async() => {
+        const res = await getReviewList(id)
+        setReviewList(res)
+    }, [])
+
+    return (
+        <div>
+            <h1>Classroom { id }</h1>
+            {reviewList.map((review) => {
+                console.log(review)
+                return(
+                    <div>
+                        {review}.rating
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
 
 export default Classroom;
